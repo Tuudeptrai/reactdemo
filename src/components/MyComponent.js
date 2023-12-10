dcede1e000f9d13b11a46c8d6dc4b0a66c704f74
Tuudeptrai <vuvantuu.gl@gmail.com>
2023-12-10 15:06:07 +0700

    first commit

---
+++ src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+ class MyComponent extends React.Component{
+    //jsx
+    render(){
+        return (
+            <div>My fisrt Component
+                {Math.random()}
+            </div>
+        );
+    }
+}
+export default MyComponent;
