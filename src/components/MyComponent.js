45b32f33619615786f0c0016bec88dde9bbfed84
Tuudeptrai <vuvantuu.gl@gmail.com>
2023-12-10 16:19:47 +0700

    fix bug this by arrow function

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,11 +1,26 @@
 import React from "react";
 
  class MyComponent extends React.Component{
+    state = {
+        name: 'tuu',
+        address: "gia lai",
+        age:30
+
+    }
     //jsx
+    handleClick = (event)=>{
+        console.log('>>tuu dep trai vai lol');
+        console.log('>>tuu dep trai vai lol',this.state.name );
+        // console.log(event);
+    }
+    handleOnmouseOver(event){
+        console.log(event);
+    }
     render(){
         return (
-            <div>My fisrt Component
-                {Math.random()}
+            <div>My name is {this.state.name} and i am from {this.state.address}
+            <button onClick={this.handleClick}>Click me</button>
+            <button onMouseOver={this.handleOnmouseOver}>on mouse me</button>
             </div>
         );
     }
