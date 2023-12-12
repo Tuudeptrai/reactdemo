978248e467fda094cd4089517b5d10b674eb3a8b
Tuudeptrai <vuvantuu.gl@gmail.com>
2023-12-12 20:11:33 +0700

    OUtlet nav

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+import { Link ,Outlet } from 'react-router-dom';
 import './App.scss';
 import Header from './components/Header/Header';
 
@@ -6,16 +6,16 @@ const App = (props) => {
   return (
     <>
       <div className='container'>
-        <Header/>
-        <div>test link</div>
-        <br/>
-        <br/>
-        <div>
-          <button className='btn btn-warning'> <Link to="/users">Go to User</Link></button>
-         
-          <br/>
-          <br/>
-          <button className='btn btn-warning'> <Link to="/admins">Go to Admin</Link></button>
+        <div className='header-container'>    
+            <Header/>
+        </div>
+        <div className='main-container'>
+             <div className='sidenav-container'>
+
+             </div>
+             <div className='app-content'>
+                  <Outlet/>
+             </div>
         </div>
       </div>
     </>
