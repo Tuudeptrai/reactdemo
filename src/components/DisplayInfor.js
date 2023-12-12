08146d13845b3a8c9192c2a55ed2c6481ecbccfd
Tuudeptrai <vuvantuu.gl@gmail.com>
2023-12-12 14:40:43 +0700

    class to fucntion

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -2,64 +2,89 @@ import React from "react";
 import './DisplayInfor.scss';
 import logo from '../logo.svg';
 
-class DisplayInfor extends React.Component{
+// class DisplayInfor extends React.Component{
 
-    constructor(props){
-        console.log("alo constructor ")
-        super(props);
-        this.state = {
-            isShowListUser: true
-        }
-    }
-    state = {
-        isShowListUser: true
-    }
+//     constructor(props){
+//         console.log("alo constructor ")
+//         super(props);
+//         this.state = {
+//             isShowListUser: true
+//         }
+//     }
+//     state = {
+//         isShowListUser: true
+//     }
 
-    handleShowHide(){
-        this.setState({
-            isShowListUser: !this.state.isShowListUser
-        })
-    }
+//     handleShowHide(){
+//         this.setState({
+//             isShowListUser: !this.state.isShowListUser
+//         })
+//     }
 
-    componentDidMount(){
-        console.log("alo componentDidMount ")
-        setTimeout(()=>{
-            document.title = "componentDidMount"
-        },300)
+//     componentDidMount(){
+//         console.log("alo componentDidMount ")
+//         setTimeout(()=>{
+//             document.title = "componentDidMount"
+//         },300)
        
-    }
+//     }
 
-    componentDidUpdate(prevProps, prevState){
-        document.title = "componentDidUpdate"
-        console.log("alo componentDidUpdate ")
-    }
-    render(){
+//     componentDidUpdate(prevProps, prevState){
+//         document.title = "componentDidUpdate"
+//         console.log("alo componentDidUpdate ")
+//     }
+//     render(){
 
-        console.log("alo render ")
+//         console.log("alo render ")
         
-        const {listUsers} = this.props;
+//         const {listUsers} = this.props;
 
-        return (
-            <div className="display-infor-container">
-                <div><span className="blue" onClick={()=>{this.handleShowHide()}}>{this.state.isShowListUser===true?"hide user list":"show user list"}</span></div>
-                <hr/>
-                {this.state.isShowListUser &&
-                <>
-                    { listUsers.map((user)=>{
-                            return (
-                                <div key={user.id} className={+user.age > 18?"green":"red"}>
-                                    <p>My name is {user.name}</p>
-                                    <p>My age is {user.age}</p>
-                                    <button onClick={()=>{this.props.handleDelnewUser(user.id)}}>Delete</button>
-                                    <hr/>
-                                </div>
-                            )
-                    })}
-                </>
-                 }
-            </div>
-        )
-    }
+//         return (
+//             <div className="display-infor-container">
+//                 <div><span className="blue" onClick={()=>{this.handleShowHide()}}>{this.state.isShowListUser===true?"hide user list":"show user list"}</span></div>
+//                 <hr/>
+//                 {this.state.isShowListUser &&
+//                 <>
+//                     { listUsers.map((user)=>{
+//                             return (
+//                                 <div key={user.id} className={+user.age > 18?"green":"red"}>
+//                                     <p>My name is {user.name}</p>
+//                                     <p>My age is {user.age}</p>
+//                                     <button onClick={()=>{this.props.handleDelnewUser(user.id)}}>Delete</button>
+//                                     <hr/>
+//                                 </div>
+//                             )
+//                     })}
+//                 </>
+//                  }
+//             </div>
+//         )
+//     }
+// }
+const DisplayInfor = (props)=>{
+    
+                console.log("alo render ")
+                
+                const {listUsers} = props;
+        
+                return (
+                    <div className="display-infor-container">
+                        
+                        {true &&
+                        <>
+                            { listUsers.map((user)=>{
+                                    return (
+                                        <div key={user.id} className={+user.age > 18?"green":"red"}>
+                                            <p>My name is {user.name}</p>
+                                            <p>My age is {user.age}</p>
+                                            <button onClick={()=>{props.handleDelnewUser(user.id)}}>Delete</button>
+                                            <hr/>
+                                        </div>
+                                    )
+                            })}
+                        </>
+                         }
+                    </div>
+                )
 }
-
 export default DisplayInfor;
