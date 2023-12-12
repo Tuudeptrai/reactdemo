f6638760637adeb2e6195c0f6c81a0a3f86b512d
Tuudeptrai <vuvantuu.gl@gmail.com>
2023-12-12 19:28:43 +0700

    first nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.scss';
+import Header from './components/Header/Header';
 
 const App = (props) => {
 
-  
+
   return (
     <>
-      <div></div>
+      <div className='app-container'>
+        <Header/>
+      </div>
     </>
   )
 }
